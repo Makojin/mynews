6aff8960e54cd87282547cded4c1689afb4b3caa
Makoto <makottchan1117@gmail.com>
2020-04-15 07:55:01 +0000

    kadai

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -12,3 +12,16 @@
 </html
   </head>
 </html>
+{{-- layouts/admin.blade.phpを読み込み --}}
+@extends ('layouts.admin')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section ('content')
+   <div class="container">
+     <div class="row">
+       <div class="col-md-8 mx-auto">
+         <h2>ニュース新規作成</h2>
+       </div> 
+     </div>
+   </div>
+@endsection
