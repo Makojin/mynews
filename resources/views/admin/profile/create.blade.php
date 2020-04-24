601da5e5b522daac0dcbb00b66dc611a855a2acc
Makoto <makottchan1117@gmail.com>
2020-04-24 08:29:25 +0000

    kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.profile')
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'Myprofile')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section ('content')
@@ -12,7 +12,7 @@
          <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
 
 
-@if (count($errors) > 0)
+                        @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
                                 <li>{{ $e }}</li>
@@ -40,7 +40,7 @@
                     <div class="form-group row">
                        <label class="col-md-2">自己紹介欄</label>
                        <div class="col-md-10">
-                       　<textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
+                       　<textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                     </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
