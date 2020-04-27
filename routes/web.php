4145a7b27eb563015ae5d4c8a4dca7c65489478a
Takeshi Osawa <32290675+takeshi-osawa@users.noreply.github.com>
2020-04-27 23:04:52 +0900

    PHP/Laravel 13 課題3
    ->middleware('auth');  は、groupで共通設定しているので個別につける必要はないですね！

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin','middleware'=>'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/create','Admin\ProfileController@add');
     Route::post('profile/create','Admin\ProfileController@create');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit');
     Route::post('profile/edit','Admin\ProfileController@update');
 });
 
