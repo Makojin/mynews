112c924fb2c9c5333091858e4296fd5f2da9502b
Makoto <makottchan1117@gmail.com>
2020-05-01 01:05:14 +0000

    kadai

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,19 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin','middleware'=>'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/create','Admin\ProfileController@add');
     Route::post('profile/create','Admin\ProfileController@create');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index'); //ニュース一覧
+    Route::get('profile', 'Admin\ProfileController@index'); //プロフィール一覧
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
+    Route::get('profile/edit','Admin\ProfileController@edit');
     Route::post('profile/edit','Admin\ProfileController@update');
-});
+    Route::get('profile/delete', 'Admin\ProfileController@delete');
+    
 
+});
 
 Auth::routes();
 
